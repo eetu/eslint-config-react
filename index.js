0f5b8c18b5a5a23433424b0bffb42ab289a6523f
Eetu Sutinen <eetu.sutinen@gmail.com>
2024-08-26 14:43:47 +0300

    Add react-hooks rules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import eslintNodeConfig from "eslint-config-node";
 import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
 import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
@@ -15,12 +16,15 @@ export default [
     },
     plugins: {
       "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
     },
     rules: {
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
